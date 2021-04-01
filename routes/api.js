eb6ad75c64a192d45359ed656b8ca62bb4cfbfb8
lauracejas <lauracejasmartin94@gmail.com>
2021-04-01 19:46:38 -0400

    create api routes

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+const Workout = require("../models/workout.js");
+const express = require("express");
+const router = express.Router();
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.findOneAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }); 
+
+  router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }); 
+
+module.exports = router;
